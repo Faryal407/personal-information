dbfa73ea91f56c2cf597f454eb1b29566d4e16f2
Faryal Web Developer <faryal77779@gmail.com>
2025-11-19 00:04:23 +0500

    Refactor userDate function and update font style

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function userDate(name, fatherName, rollNo, age, course) {//perimeter
-    document.writeln(`<center><font face="Bolditalic" size="7" color="mehroon"><h1>Information </h1></font> 
+    document.writeln(`<center><font face="Broadway" size="6" color="mehroon"><h1>Information </h1></font> 
         <h1>User Name: ${name} </h1> <h1>Father Name: ${fatherName} </h1>
          <h1>Roll No: ${rollNo} </h1> <h1>Age: ${age} </h1> <h1>Course: ${course} </h1> </center>`);
 
@@ -7,4 +7,5 @@ function userDate(name, fatherName, rollNo, age, course) {//perimeter
 userDate(prompt("Enter Your Name:"), prompt("Enter Your Father Name:"), prompt("Enter Your Roll No:"), 
 
 prompt("Enter Your Age:"), ["Web Development", "AI"]);//argument
+
 
