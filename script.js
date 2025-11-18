31d3f6405367cf00acd24c1863a56978d9f150da
Faryal Web Developer <faryal77779@gmail.com>
2025-11-18 20:20:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function userDate(name, fatherName, rollNo, age, course) {//perimeter
+    document.writeln(`<center><font face="Bolditalic" size="7" color="mehroon"><h1>My Information </h1></font> 
+        <h1>User Name: ${name} </h1> <h1>Father Name: ${fatherName} </h1>
+         <h1>Roll No: ${rollNo} </h1> <h1>Age: ${age} </h1> <h1>Course: ${course} </h1> </center>`);
+
+}
+userDate(prompt("Enter Your Name:"), prompt("Enter Your Father Name:"), prompt("Enter Your Roll No:"), 
+prompt("Enter Your Age:"), ["Web Development", "AI"]);//argument
